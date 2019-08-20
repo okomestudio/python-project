4fe64f6e59fd9f6837471d90d39f5752471cffe0
Taro Sato <okomestudio@gmail.com>
2019-08-20 15:36:00 -0700

    Add basic files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # python-project
+
 Project template for Python
---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+import codecs
+import os
+import re
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+def meta(category, fpath="src/package/__init__.py"):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, fpath), "r") as f:
+        package_root_file = f.read()
+    matched = re.search(
+        r"^__{}__\s+=\s+['\"]([^'\"]*)['\"]".format(category), package_root_file, re.M
+    )
+    if matched:
+        return matched.group(1)
+    raise Exception("Meta info string for {} undefined".format(category))
+
+
+requires = []
+
+setup_requires = ["pre-commit"]
+
+test_requires = ["pytest"]
+
+setup(
+    name="python-project",
+    version=meta("version"),
+    description="Project description",
+    author=meta("author"),
+    url="https://github.com/okomestudio/python-project",
+    platforms=["Linux"],
+    classifiers=[],
+    package_dir={"": "src"},
+    packages=find_packages("src"),
+    python_requires=">=3.7",
+    scripts=[],
+    install_requires=requires,
+    setup_requires=setup_requires,
+    tests_require=test_requires,
+    extras_require={"test": test_requires},
+    entry_points={"console_scripts": []},
+)
